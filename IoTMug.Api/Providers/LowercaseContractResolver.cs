89dd32c4ad8e86624cb2e4c4938e58ec6f243f7d
Bolchert <david.bolchert@gmail.com>
2019-10-13 19:13:47 +0200

    add entity framework

---
+++ IoTMug.Api/Providers/LowercaseContractResolver.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace IoTMug.Api.Providers
+{
+    public class LowercaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName) || char.IsLower(propertyName, 0))
+                return propertyName;
+
+            return char.ToLowerInvariant(propertyName[0]) + propertyName.Substring(1);
+        }
+    }
+}
--- IoTMug.Api/Startup.cs
+++ IoTMug.Api/Startup.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using IoTMug.Api.Providers;
+using IoTMug.Data;
+using IoTMug.Data.Repositories;
+using IoTMug.Services.Interfaces;
+using IoTMug.Services.Shared;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace Meetup.Api
 {
@@ -18,8 +25,21 @@ namespace Meetup.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DatabaseSettings>(Configuration.GetSection("ConnectionStrings"));
+
+            // Add entity framework services.
+            services.AddDbContext<IoTMugContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IDatabaseService, GenericRepository>();
+
             services.AddCors();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new LowercaseContractResolver();
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
